4e062741163a9849dca77f1be37c788db74b66ac
SergeySherbakov <serega.alexandr@yandex.ru>
2022-11-03 00:14:00 +0700

    добавил программу

---
+++ task1/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array1;
+Console.WriteLine("Введите массив строк через пробел:");
+var userString = Console.ReadLine();
+array1 = userString!.Split(' ');
+
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArray(array1, array2);
+PrintArray(array2);
